94a919d07d3cbacd854f8225b7e64270d9b1416e
Nick Elser <nick.elser@gmail.com>
2016-06-29 17:04:44 -0700

    Run coverage the right time.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,13 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-
-require "active_job/traffic_control"
-
-require "minitest/autorun"
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 if ENV["CODECLIMATE_REPO_TOKEN"]
   require "codeclimate-test-reporter"
   ::SimpleCov.add_filter "helper"
   CodeClimate::TestReporter.start
 end
+
+require "active_job/traffic_control"
+require "minitest/autorun"
 
 def test_logger
   @logger ||= begin
